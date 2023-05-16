94ec6eff9bdac5c2fe9a8413ea63f7111bb6ccee
ernest korir <ernestbore3@gmail.com>
2023-05-16 18:04:42 +0300

    Make Changes on Location on Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
