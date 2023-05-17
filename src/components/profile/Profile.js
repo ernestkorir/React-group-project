910e4ccf84157dfd29e5993eeef12d6c72dbe6ce
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-17 09:23:37 +0300

    :hammer:Fix linters

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,28 +4,27 @@ import './Profile.css';
 const Profile = () => {
   const rocketList = useSelector((state) => state.rocket);
   const rocketFiltered = rocketList.rocket.filter((rocket) => rocket.reserved === true);
-  const allMissions = useSelector((state) => state.mission.missions)
-  const joinedMissions = allMissions.filter((mission) => mission.reserved === true)
+  const allMissions = useSelector((state) => state.mission.missions);
+  const joinedMissions = allMissions.filter((mission) => mission.reserved === true);
 
-  
   return (
     <section className="profilePage">
         <div className='mission-reserve'>
-  <h2>My Missions</h2>
-  <table className='tmission'>
-    <tbody>
-      { joinedMissions.length > 0 ? (
-        joinedMissions.map((joined) => (
-          <tr key={joined.mission_id}>
-            <td>{joined.mission_name}</td>
-          </tr>
-        ))
-      ) : (
-        <p>No Mission Joined</p>
-      )}
-    </tbody>
-  </table>
-</div>
+          <h2>My Missions</h2>
+          <table className='tmission'>
+            <tbody>
+              { joinedMissions.length > 0 ? (
+                joinedMissions.map((joined) => (
+                <tr key={joined.mission_id}>
+                  <td>{joined.mission_name}</td>
+                </tr>
+                ))
+              ) : (
+                <p>No Mission Joined</p>
+              )}
+            </tbody>
+          </table>
+      </div>
       <div className='rocket-reserve'>
         <h2> My Rockects</h2>
         <table className='trocket'>
