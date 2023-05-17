37c7b662562c89d373cfdcb6dd213f249af3c578
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-17 08:10:37 +0300

    :sparkles:Add and display the joined missions

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,8 +4,28 @@ import './Profile.css';
 const Profile = () => {
   const rocketList = useSelector((state) => state.rocket);
   const rocketFiltered = rocketList.rocket.filter((rocket) => rocket.reserved === true);
+  const allMissions = useSelector((state) => state.mission.missions)
+  const joinedMissions = allMissions.filter((mission) => mission.reserved === true)
+
+  
   return (
     <section className="profilePage">
+        <div>
+  <h2>My Missions</h2>
+  <table>
+    <tbody>
+      { joinedMissions.length > 0 ? (
+        joinedMissions.map((joined) => (
+          <tr key={joined.mission_id}>
+            <td>{joined.mission_name}</td>
+          </tr>
+        ))
+      ) : (
+        <p>No Mission Joined</p>
+      )}
+    </tbody>
+  </table>
+</div>
       <div>
         <h2 className="title"> My Rockects</h2>
         <table>
